6a3b7b01f846c8660d1eb6c35a2dee6b3c00e15d
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2024-01-09 16:59:18 +0100

    Let the action zip the files
    so we don't end up with a zip zip.

--- scripts/archieveResults.py
+++ scripts/archieveResults.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 import os
+import shutil
 import sys
-from zipfile import ZipFile
 
-def zipFiles(libraryName, branchLib, branchOM, omLibTestingDir, zipFile):
+def copyFiles(libraryName, branchLib, branchOM, omLibTestingDir, targetDir):
   """
   Zip HTML files and save in target directory.
 
@@ -13,32 +13,25 @@ def zipFiles(libraryName, branchLib, branchOM, omLibTestingDir, zipFile):
   branchLib       -- Branch or version of Modelica library.
   branchOM        -- Branch or version of OpenModelica.
   omLibTestingDir -- Root directory of OpenModelicaLibraryTesting repository.
-  zipFile         -- Target directory for zip file.
+  targetDir       -- Target directory.
   """
 
   libNameBranch = f"{libraryName}_{branchLib}"
 
-  with ZipFile(zipFile, 'w') as zip:
-    # Add logs and diffs
-    for path, _, files in os.walk(os.path.join(omLibTestingDir, "files")):
-       for file in files:
-          zip.write(os.path.join(path, file),
-                    arcname=os.path.join(branchOM, libNameBranch, "files", file))
+  shutil.copytree(os.path.join(omLibTestingDir, "files"),
+                  os.path.join(targetDir, branchOM, libNameBranch, "files"),
+                  dirs_exist_ok = True)
 
-    # Add overview
-    zip.write(os.path.join(omLibTestingDir, "overview.html"),
-              arcname="index.html")
+  shutil.copy2(os.path.join(omLibTestingDir, "overview.html"),
+               os.path.join(targetDir, "index.html"))
 
-    # Add library overview
-    zip.write(os.path.join(omLibTestingDir, f"{libNameBranch}.html"),
-              arcname=os.path.join(branchOM, libNameBranch, f"{libNameBranch}.html"))
+  # Copy library overview
+  shutil.copy2(os.path.join(omLibTestingDir, f"{libNameBranch}.html"),
+               os.path.join(targetDir, branchOM, libNameBranch, f"{libNameBranch}.html"))
 
-    # Add diff javascript
-    zip.write(os.path.join(os.path.dirname(__file__), "dygraph-combined.js"),
-              arcname=os.path.join(branchOM, libNameBranch, "files", "dygraph-combined.js"))
-
-    zip.printdir()
-
+  # Copy dygraph script
+  shutil.copy2(os.path.join(os.path.dirname(__file__), "dygraph-combined.js"),
+               os.path.join(targetDir, branchOM, libNameBranch, "files", "dygraph-combined.js"))
 
 if len(sys.argv) != 6:
   raise Exception("Wrong number of input arguments.\nUsage:\narchieveResults.py libraryName libVersion branchOM omLibTestingDir zipFile")
@@ -47,11 +40,10 @@ libraryName     = sys.argv[1]
 libVersion      = sys.argv[2]
 branchOM        = sys.argv[3]
 omLibTestingDir = sys.argv[4]
-zipFile         = sys.argv[5]
+targetDir       = sys.argv[5]
 
 # If running inside a pull request
 if libVersion.endswith('/merge'):
   libVersion = 'dev-pr-' + libVersion.replace('/merge', '')
 
-
-zipFiles(libraryName, libVersion, branchOM, omLibTestingDir, zipFile)
+copyFiles(libraryName, libVersion, branchOM, omLibTestingDir, targetDir)
